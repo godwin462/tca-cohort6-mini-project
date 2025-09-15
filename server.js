88f2b2100f09aaf24346424384e8e17a124a2e61
vibeofficial <ichioguchristoper98@gmail.com>
2025-09-15 15:24:14 +0100

    first commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(baseUrl, (req, res) => {
-  res.status(200).json('Connected to Backend API')
+  res.send('Connected to Backend Server')
 });
 app.use('/api/v1', userRouter);
 
